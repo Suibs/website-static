2f57bd0eb5a99d99d927df7e39b719eeb01c5982
Suibin Hong <suibin.hong@outlook.com>
2020-04-25 10:00:43 +0100

    Adding font size to p in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,14 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import {StaticQuery, graphql, Link} from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 
 import Header from "./header"
 import "../../static/styles/main.scss"
 import logo from "../../static/images/logo.svg"
 import iconTwitter from "../../static/images/icon-twitter.svg"
 
-const Layout = ({children}) => (
+const Layout = ({ children }) => (
     <StaticQuery query={graphql`
       query SiteTitleQuery {
         site {
@@ -24,55 +24,55 @@ const Layout = ({children}) => (
         }
       }
     `} render={data => (
-        <>
-            <Header siteTitle={data.site.siteMetadata.title}/>
-            <main>{children}</main>
-            <footer>
-                <div className={"container"}>
-                    <div className={"row"}>
-                        <div className={"col-6"}>
-                            <div className={"widget__item"}>
-                                <div className={"logo"}>
-                                    <Link to="/" title={"Tree Style Tab"}>
-                                        <img alt={"Logo"} src={logo}/>
-                                    </Link>
-                                </div>
+            <>
+                <Header siteTitle={data.site.siteMetadata.title} />
+                <main>{children}</main>
+                <footer>
+                    <div className={"container"}>
+                        <div className={"row"}>
+                            <div className={"col-6"}>
+                                <div className={"widget__item"}>
+                                    <div className={"logo"}>
+                                        <Link to="/" title={"Tree Style Tab"}>
+                                            <img alt={"Logo"} src={logo} />
+                                        </Link>
+                                    </div>
 
-                                <div className={"about"}>
-                                    <p>A google extension that utilises the concept of tree-like hireachy structure for tab management.</p>
+                                    <div className={"about"}>
+                                        <p>A google extension that utilises the concept of tree-like hireachy structure for tab management.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={"col-2"}>
+                                <div className={"widget__item"}>
+                                    <div className={"links"}>
+                                        <h4>Support</h4>
+                                        <ul>
+                                            <li><Link to="/contact" title={"Contact Us"}>Contact</Link></li>
+                                            <li><Link to="/privacy" title={"Privacy Policy"}>Privacy</Link></li>
+                                            <li><Link to="/terms" title={"Terms Of Use"}>Terms Of Use</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={"col-4"}>
+                                <div className={"widget__item"}>
+                                    <div className={"social"}>
+                                        <a href="https://twitter.com/treestyletab" target={"_blank"} title={"Twitter"}><img alt={"Twitter"} src={iconTwitter} /></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div className={"col-2"}>
-                            <div className={"widget__item"}>
-                                <div className={"links"}>
-                                    <h4>Support</h4>
-                                    <ul>
-                                        <li><Link to="/contact" title={"Contact Us"}>Contact</Link></li>
-                                        <li><Link to="/privacy" title={"Privacy Policy"}>Privacy</Link></li>
-                                        <li><Link to="/terms" title={"Terms Of Use"}>Terms Of Use</Link></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={"col-4"}>
-                            <div className={"widget__item"}>
-                                <div className={"social"}>
-                                    <a href="https://twitter.com/treestyletab" target={"_blank"} title={"Twitter"}><img alt={"Twitter"} src={iconTwitter}/></a>
-                                </div>
-                            </div>
+                        <div className={"copyright"}>
+                            <p>Copyright {new Date().getFullYear()}, {` `} <a href="/" title={"Tree Style Tab"}>Tree Style Tab</a>. All rights reserved.</p>
                         </div>
                     </div>
-
-                    <div className={"copyright"}>
-                        <p>Copyright {new Date().getFullYear()}, {` `} <a href="/" title={"Tree Style Tab"}>Tree Style Tab</a>. All rights reserved.</p>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )}/>
+                </footer>
+            </>
+        )} />
 )
 
 Layout.propTypes = {
