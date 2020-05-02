3ff0fcfa5db9ef30a2d1e6f5b0c7f566d7cbec0c
Suibin Hong <suibin.hong@outlook.com>
2020-05-02 19:41:37 +0100

    Test GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,25 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-                // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+                // The property ID; the tracking code won't be generated without it
                 trackingId: "G-1H3QGQJ70R",
-                head: true
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                // Defers execution of google analytics script after page load
+                defer: false,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: "treestyletab.com",
             },
         },
         `gatsby-plugin-react-helmet`,
