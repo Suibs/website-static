32113e52831aa5e4c28062da60d44489ae045398
Suibin Hong <suibin.hong@outlook.com>
2020-05-02 19:48:08 +0100

    Using gtag manager instead

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,27 +6,20 @@ module.exports = {
     },
     plugins: [
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: "gatsby-plugin-google-tagmanager",
             options: {
-                // The property ID; the tracking code won't be generated without it
-                trackingId: "G-1H3QGQJ70R",
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: ["/preview/**", "/do-not-track/me/too/"],
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
-                // Enables Google Optimize using your container Id
-                // Defers execution of google analytics script after page load
-                defer: false,
-                // Any additional optional fields
-                sampleRate: 5,
-                siteSpeedSampleRate: 10,
-                cookieDomain: "treestyletab.com",
+                id: "G-1H3QGQJ70R",
+
+                // Include GTM in development.
+                // Defaults to false meaning GTM will only be loaded in production.
+                includeInDevelopment: false,
+
+                // datalayer to be set before GTM is loaded
+                // should be an object or a function that is executed in the browser
+                // Defaults to null
+                defaultDataLayer: { platform: "gatsby" },
+
+                // Specify optional GTM environment details.
             },
         },
         `gatsby-plugin-react-helmet`,
